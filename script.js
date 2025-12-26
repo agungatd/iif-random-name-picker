3cfc38a3f77bdf5b2eeb94aa76628f6734f5d468
agungatd <agungatidhira@gmail.com>
2025-12-26 21:46:26 +0700

    update script

--- script.js
+++ script.js
@@ -311,9 +311,6 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function shuffleArray(array) {
-    // set seed for reproducibility
-    // const seed = 42;
-    // Math.seed = seed;
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
